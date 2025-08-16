02cd822a2c35bf1252183b1eb33865aeac360188
jagtapK <jagtapkajal54@gmail.com>
2025-08-16 22:11:46 +0530

    Adding new method

--- src/main/java/com/WebServicesProject/Fullstack_Backend/controller/UserController.java
+++ src/main/java/com/WebServicesProject/Fullstack_Backend/controller/UserController.java
@@ -4,14 +4,14 @@ import com.WebServicesProject.Fullstack_Backend.model.User;
 import com.WebServicesProject.Fullstack_Backend.repository.UserRepository;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:5173")
 public class UserController {
 
     @Autowired
@@ -27,5 +27,11 @@ public class UserController {
         return userRepository.findAll();
     }
 
+
+    @DeleteMapping("/deleteAll")
+    public ResponseEntity<String> deleteAllUsers() {
+        userRepository.deleteAll();
+        return ResponseEntity.ok("All users have been deleted successfully.");
+    }
 
 }
