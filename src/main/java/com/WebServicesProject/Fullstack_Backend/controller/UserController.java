14f5fa8cbb22723015f50726e81942441cdd5b57
jagtapK <jagtapkajal54@gmail.com>
2025-08-29 15:32:30 +0530

    updated

--- src/main/java/com/WebServicesProject/Fullstack_Backend/controller/UserController.java
+++ src/main/java/com/WebServicesProject/Fullstack_Backend/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:5173")
 @RequestMapping
 public class UserController {
 
